d165519209b9e829cadae0f24402b28306fee556
weics <weics.cn@gmail.com>
2018-05-12 17:43:42 +0800

    消息的控制系统

---
+++ weii-pay/weii-pay-web-message/src/main/java/com/weii/pay/controller/common/BaseController.java
@@ -0,0 +1,42 @@
+
+package com.weii.pay.controller.common;
+
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public abstract class BaseController {
+
+//	/**
+//	 * 获取request
+//	 *
+//	 * @return
+//	 */
+//	protected HttpServletRequest getRequest() {
+//		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+//	}
+//
+//	/**
+//	 * 获取session
+//	 *
+//	 * @return
+//	 */
+//	protected HttpSession getSession() {
+//		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession();
+//	}
+//
+//	/**
+//	 * 获取application
+//	 *
+//	 * @return
+//	 */
+//	protected ServletContext getApplication() {
+//		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession().getServletContext();
+//	}
+//
+//	protected ServletContext getServletContext() {
+//		return ContextLoader.getCurrentWebApplicationContext().getServletContext();
+//	}
+
+}
