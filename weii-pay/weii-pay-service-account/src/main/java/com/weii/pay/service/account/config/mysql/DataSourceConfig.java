e979d5606a7fd754c7cfe84f0d392d2fcda22b61
weics <weics.cn@gmail.com>
2018-08-19 19:55:49 +0800

    更新

---
+++ weii-pay/weii-pay-service-account/src/main/java/com/weii/pay/service/account/config/mysql/DataSourceConfig.java
@@ -0,0 +1,10 @@
+package com.weii.pay.service.account.config.mysql;
+
+/**
+ * @Author: weics
+ * @Date: Created in 19:13 2018/8/19
+ * @Description:
+ * @Modified By:
+ */
+public class DataSourceConfig {
+}
